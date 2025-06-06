0630b25fe2a667d438946e93ee2fe9d160104095
sammytamimi <89275325+sammytamimi@users.noreply.github.com>
2025-06-06 17:48:29 +0100

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className="bg-white text-gray-900 min-h-screen flex flex-col">
         <Header />
         <main className="flex-grow">{children}</main>
+        <Analytics />
         <Footer />
       </body>
     </html>
