601eccb43d554763a944d34385e2f1b3970ec7f4
sammytamimi <89275325+sammytamimi@users.noreply.github.com>
2025-06-06 16:28:34 +0100

    add titles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow container mx-auto px-4 sm:px-6 py-16 max-w-6xl">
-
+        <h1 className="text-4xl md:text-5xl font-bold mb-12 leading-tight text-center md:text-left">About</h1>
         
         {/* Profile Section */}
         <div>
