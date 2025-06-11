75039cec0007dd710279bd0e21103f50ef6a57e0
sammytamimi <89275325+sammytamimi@users.noreply.github.com>
2025-06-11 15:18:56 +0100

    update wording

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const FOUNDER_PUBS_HEADING = "Articles by Our Founder Published In";
+const FOUNDER_PUBS_HEADING = "Founder Published In";
 
 export default function Home() {
   return (
